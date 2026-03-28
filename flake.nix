28c8f9ef2b50f44f35764e0e0a61c46eab5bd7c0
Uzair Aftab <git@uzaaft.me>
2026-03-28 15:38:45 +0100

    build: setup nix and cmake baseline based upon ghostling

--- flake.nix
+++ flake.nix
@@ -1,83 +1,62 @@
 {
   description = "C++17 CMake scaffold for libghostty-cpp";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      lib = nixpkgs.lib;
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        lib.genAttrs supportedSystems (
-          system:
-          f {
-            inherit system;
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
-    in
     {
-      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt-tree);
+      self,
+      nixpkgs,
+      flake-utils,
+      zig,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        formatter = pkgs.nixfmt-tree;
 
-      packages = forEachSupportedSystem (
-        { pkgs, ... }:
-        {
-          default = pkgs.stdenv.mkDerivation {
-            pname = "libghostty-cpp";
-            version = "0.1.0";
-            src = lib.cleanSource ./.;
+        devShells.default = pkgs.mkShell {
+          packages = [
+            zig.packages.${system}."0.15.2"
+            pkgs.clang-tools
+            pkgs.cmake
+            pkgs.ninja
+            pkgs.pinact
+            pkgs.pkg-config
+            pkgs.scc
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+            pkgs.xorg.libX11
+            pkgs.xorg.libXcursor
+            pkgs.xorg.libXrandr
+            pkgs.xorg.libXinerama
+            pkgs.xorg.libXi
+            pkgs.libGL
+            pkgs.libxkbcommon
+            pkgs.wayland
+          ];
 
-            nativeBuildInputs = with pkgs; [
-              cmake
-              ninja
-              pkg-config
-            ];
-
-            cmakeFlags = [ "-DCMAKE_BUILD_TYPE=Release" ];
-          };
-        }
-      );
-
-      devShells = forEachSupportedSystem (
-        { pkgs, system, ... }:
-        {
-          default = pkgs.mkShell {
-            packages =
-              (with pkgs; [
-                clang-tools
-                cmake
-                ninja
-                pkg-config
-                stdenv.cc
-                self.formatter.${system}
-              ])
-              ++ lib.optionals pkgs.stdenv.isLinux [ pkgs.gdb ]
-              ++ lib.optionals pkgs.stdenv.isDarwin [ pkgs.lldb ];
-
-            env = {
-              CMAKE_BUILD_PARALLEL_LEVEL = "8";
-              CTEST_OUTPUT_ON_FAILURE = "1";
-            };
-
-            shellHook = ''
-              export CMAKE_GENERATOR=Ninja
-            '';
-          };
-        }
-      );
-
-      checks = forEachSupportedSystem (
-        { system, ... }:
-        {
-          default = self.packages.${system}.default;
-        }
-      );
-    };
+          # Unset Nix Darwin SDK env vars and remove xcbuild
+          # xcrun wrapper so Zig's SDK detection uses the real
+          # system xcrun/xcode-select
+          shellHook = ''
+            export CMAKE_GENERATOR=Ninja
+            unset SDKROOT
+            unset DEVELOPER_DIR
+            export PATH=$(echo "$PATH" | tr ':' '\n' | grep -v -e xcbuild -e apple-sdk | tr '\n' ':')
+          '';
+        };
+      }
+    );
 }
